016fa78f0521e8f7f47b729d4750d559d76fc45c
Duncan Beevers <duncan@dweebd.com>
2011-05-25 08:06:48 -0700

    Multiple argument partial ordering creates a chain of dependencies

--- test/toposort.test.js
+++ test/toposort.test.js
@@ -13,30 +13,43 @@ var tests = [
 
   function testEmptySet() {
     var set = new toposort();
-    assert.deepEqual([], set.sort([]));
+    assert.deepEqual(set.sort([]), []);
   },
 
   function testSortItem() {
     var set = new toposort();
-    assert.deepEqual(['a'], set.sort(['a']));
+    assert.deepEqual(set.sort([ 'a' ]), [ 'a' ]);
   },
 
   function testFallbackSort() {
     var set = new toposort();
-    assert.deepEqual([ 'a', 'b' ], set.sort([ 'a', 'b' ], sortAlphabetically));
-    assert.deepEqual([ 'b', 'a' ], set.sort([ 'a', 'b' ], sortReverseAlphabetically));
+    assert.deepEqual(
+      set.sort([ 'a', 'b' ], sortAlphabetically),
+      [ 'a', 'b' ]);
+    assert.deepEqual(
+      set.sort([ 'a', 'b' ], sortReverseAlphabetically),
+      [ 'b', 'a' ]);
   },
 
   function testAddPartialOrdering() {
     var set = new toposort();
-    assert.deepEqual([ 'a', 'b' ],
-      set.addPartialOrdering('a', 'b').sort([ 'a', 'b' ], sortReverseAlphabetically));
+    assert.deepEqual(
+      set.addPartialOrdering('a', 'b').sort([ 'a', 'b' ], sortReverseAlphabetically),
+      [ 'a', 'b' ]);
   },
 
   function testAddPartialOrderingWithFallbackSort() {
     var set = new toposort().addPartialOrdering('a', 'b');
-    assert.deepEqual([ 'c', 'a', 'b' ],
-      set.sort([ 'a', 'b', 'c' ], sortReverseAlphabetically));
+    assert.deepEqual(
+      set.sort([ 'a', 'b', 'c' ], sortReverseAlphabetically),
+      [ 'c', 'a', 'b' ]);
+  },
+
+  function testSparseAddPartialOrdering() {
+    var set = new toposort().addPartialOrdering('a', 'c', 'e');
+    assert.deepEqual(
+      set.sort([ 'a', 'b', 'c', 'd', 'e', 'f' ], sortReverseAlphabetically),
+     ['f', 'a', 'c', 'e', 'd', 'b' ]);
   }
 ];
 
